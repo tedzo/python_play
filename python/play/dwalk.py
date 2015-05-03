0128ca2edb48cb58c8a68b4b6e9a8eaeba53518c
Ted Wilcox <ted@purestorage.com>
2015-05-03 12:43:46 -0700

    Convert tabs to spaces, and set up vim modeline to expand tabs going forward.

--- python/play/dwalk.py
+++ python/play/dwalk.py
@@ -1,5 +1,7 @@
 #! /usr/bin/env python
 
+# vim: set sw=4 ai et sm:
+
 """dwalk: walk a directory tree, printing entries hierarchically
    + top
    | > file
@@ -24,17 +26,17 @@ def dwalk(path, header=''):
     files = []
     dirs = []
     for e in os.listdir(path):
-	epath = os.path.join(path, e)
+        epath = os.path.join(path, e)
         if os.path.isdir(epath):
-	    # print header + '{} is a dir'.format(e)
-	    dirs.append(e)
+            # print header + '{} is a dir'.format(e)
+            dirs.append(e)
         else:
-	    # print header + '{} is a file'.format(e)
-	    files.append(e)
+            # print header + '{} is a file'.format(e)
+            files.append(e)
     # print '{} dirs: {}'.format(header, dirs)
     # print '{} files: {}'.format(header, files)
     for f in sorted(files):
-	# print header + '| > ' + os.path.join(path, f)
+        # print header + '| > ' + os.path.join(path, f)
         print header + '| > ' + f
     for d in sorted(dirs):
         dwalk(os.path.join(path,d), header+'| ')
