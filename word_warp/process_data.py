6edba025315754e53af524da1d2dcf61c8043e2e
Ted Wilcox <tedzow@gmail.com>
2019-06-18 23:15:32 -0700

    Add Stats class to word warp processor.
    New class abstracts and improves stats gathering and printing.
    Includes a histogram of number of words per group.

--- word_warp/process_data.py
+++ word_warp/process_data.py
@@ -51,6 +51,10 @@ def line_parses(line, debug):
     else:
         return False
 
+def get_words(line):
+    """Given a line that parses, return a list of words in that line."""
+    return [word.lower() for word in line.split(', ')]
+
 def words_are_anagrams(word_list, debug):
     """Return words in list are sorted anagrams of each other."""
     # Check if all the words are the same length
@@ -89,14 +93,14 @@ def line_is_good(line, debug):
     """
     if not line_parses(line, debug):
         return (False, 0, 'Did not parse.')
-    word_list = [word.lower() for word in line.split(', ')]
+    word_list = get_words(line)
     if debug: print word_list
     if not words_are_anagrams(word_list, debug):
         return (False, 0, 'Not anagrams')
     if not words_are_alphabetic(word_list, debug):
         return (False, 0, 'Not alphabetic')
 
-    return (True, len(word_list), 'All is well')
+    return (True, 'All is well')
 
 def parse_args(args):
     """Parse command line arguments to the program."""
@@ -132,6 +136,33 @@ def get_user_lines(prompt):
             break
         yield line
 
+class Stats(object):
+    MAX_ALLOWED_WORDS = 10
+
+    def __init__(self):
+        self.good_lines = 0
+        self.good_words = 0
+        self.histogram = [0] * Stats.MAX_ALLOWED_WORDS
+        self.most_words = 0
+
+    def add_line(self, line):
+        self.good_lines += 1
+        word_list = get_words(line)
+        num_words = len(word_list)
+        assert(num_words < Stats.MAX_ALLOWED_WORDS)
+        if num_words > self.most_words:
+            self.most_words = num_words
+        self.good_words += num_words
+        self.histogram[num_words] += 1
+
+    # def show(self, indent):
+    def show(self):
+        print "  Unique 6-letter combinations: {}".format(self.good_lines)
+        print "  Unique words: {}".format(self.good_words)
+        for i in range(1, self.most_words+1):
+            print "  Number of {}-word anagrams: {}".format(i, self.histogram[i])
+
+
 def process_files(args):
     """Process lines in the files given on the command line."""
 
@@ -145,12 +176,10 @@ def process_files(args):
         #    statistics for the previous file.
         if fileinput.isfirstline():
             if args.stats and not at_beginning:
-                print "  Unique 6-letter combinations: {}".format(good_lines)
-                print "  Unique words: {}".format(good_words)
+                my_stats.show()
             last_good_line = ''
+            my_stats = Stats()
             count = 0
-            good_lines = 0
-            good_words = 0
             if args.verbose or args.stats:
                 print 'Processing "{}"'.format(fileinput.filename())
         at_beginning = False
@@ -175,7 +204,7 @@ def process_files(args):
             fileinput.nextfile()
             continue
 
-        line_good, num_words, reason = line_is_good(line, args.debug_program)
+        line_good, reason = line_is_good(line, args.debug_program)
         if not line_good:
             succeeded = False
             print 'Bad {}.{}({}): "{}"'.format(fileinput.filename(),
@@ -186,13 +215,11 @@ def process_files(args):
                     fileinput.filelineno(), line, last_good_line)
         else:
             last_good_line = line
-            good_lines += 1
-            good_words += num_words
+            my_stats.add_line(line)
 
     # Print the stats for the last file.
     if args.stats and not at_beginning:
-        print "  Unique 6-letter combinations: {}".format(good_lines)
-        print "  Unique words: {}".format(good_words)
+        my_stats.show()
 
     if succeeded:
         return 0
@@ -211,7 +238,7 @@ if __name__ == '__main__':
 
     if args.interactive:
         for line in get_user_lines('Please type a line -> '):
-            line_good, num_words, reason = line_is_good(line, args.debug_program)
+            line_good, reason = line_is_good(line, args.debug_program)
             if line_good:
                 print 'Good: "{}"'.format(line)
             else:
